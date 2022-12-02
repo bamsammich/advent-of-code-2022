cbb996a4318ee72f970bf2f252d0bc49bc1fe10f
Travis Huddleston <travis.huddleston@gmail.com>
2022-12-02 07:39:35 -0500

    day 3 prep

---
+++ day_3.go
@@ -0,0 +1,23 @@
+package main
+
+func init() {
+	puzzles = append(puzzles, &Day3Puzzle{})
+}
+
+type Day3Puzzle struct{}
+
+func (day *Day3Puzzle) Name() string {
+	return "day_3"
+}
+
+func (day *Day3Puzzle) Solution() (*Result, error) {
+	// i, err := aocutil.NewInputFromFile("session_id")
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// data, err := i.Strings(2022, 3)
+	// if err != nil {
+	// 	return nil, err
+	// }
+}
