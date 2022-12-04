6994b0b44d13d247e9613ab90736716909c50a48
Travis Huddleston <travis.huddleston@immuta.com>
2022-12-04 06:47:02 -0500

    day 4 solution

--- day_4.go
+++ day_4.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/echojc/aocutil"
@@ -16,6 +18,34 @@ func (day *Day4Puzzle) Name() string {
 	return "day_4"
 }
 
+type Section struct {
+	Start, End int
+}
+
+func (s *Section) Contains(c *Section) bool {
+	if s.Start <= c.Start && s.End >= c.End {
+		return true
+	}
+	return false
+}
+
+func (s *Section) Overlaps(c *Section) bool {
+	if s.End <= c.End && s.End >= c.Start {
+		return true
+	}
+	if s.End >= c.End && s.Start <= c.End {
+		return true
+	}
+	return false
+}
+
+func SectionFromElf(elf string) *Section {
+	sectionStr := strings.Split(elf, "-")
+	start, _ := strconv.Atoi(sectionStr[0])
+	end, _ := strconv.Atoi(sectionStr[1])
+	return &Section{start, end}
+}
+
 func (day *Day4Puzzle) Solution() (*Result, error) {
 	begin := time.Now()
 	i, err := aocutil.NewInputFromFile("session_id")
@@ -28,9 +58,23 @@ func (day *Day4Puzzle) Solution() (*Result, error) {
 		return nil, err
 	}
 
+	var countContains = 0
+	var countOverlaps = 0
+	for _, pair := range data {
+		elves := strings.Split(pair, ",")
+		elfOne := SectionFromElf(elves[0])
+		elfTwo := SectionFromElf(elves[1])
+		if elfOne.Contains(elfTwo) || elfTwo.Contains(elfOne) {
+			countContains++
+		}
+		if elfOne.Overlaps(elfTwo) {
+			countOverlaps++
+		}
+	}
+
 	return &Result{
-		First:    nil,
-		Second:   nil,
+		First:    countContains,
+		Second:   countOverlaps,
 		Duration: time.Now().Sub(begin),
 	}, nil
 }
