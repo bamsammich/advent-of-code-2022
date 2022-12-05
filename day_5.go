8d0dbf58bd95b27b476293c4dd7223a030f22368
Travis Huddleston <travis.huddleston@immuta.com>
2022-12-05 08:36:42 -0500

    day 5 solution and ignore cached input

--- day_5.go
+++ day_5.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/echojc/aocutil"
@@ -17,6 +19,10 @@ func (day *Day5Puzzle) Name() string {
 	return "day_5"
 }
 
+type day5Move struct {
+	Count, Source, Destination int
+}
+
 func (day *Day5Puzzle) Solution() (*Result, error) {
 	begin := time.Now()
 	i, err := aocutil.NewInputFromFile("session_id")
@@ -29,10 +35,79 @@ func (day *Day5Puzzle) Solution() (*Result, error) {
 		return nil, err
 	}
 
-	fmt.Println(data)
+	stacks := make([][]string, 9)
+	for i := 7; i >= 0; i-- {
+		for r := 0; r < len(data[i]); r += 4 {
+			crate := string(data[i][r : r+3])
+			if crate == "   " {
+				continue
+			}
+			stackNum := r / 4
+			if stacks[stackNum] == nil {
+				stacks[stackNum] = []string{}
+			}
+			stacks[stackNum] = append(stacks[stackNum], crate)
+		}
+	}
+
+	var moves []day5Move
+	for _, move := range data[10:] {
+		match := regexp.MustCompile(`move (\d+) from (\d+) to (\d+)`).FindAllStringSubmatch(move, 1)[0]
+		count, _ := strconv.Atoi(match[1])
+		src, _ := strconv.Atoi(match[2])
+		dest, _ := strconv.Atoi(match[3])
+		moves = append(moves, day5Move{count, src - 1, dest - 1})
+	}
+
 	return &Result{
-		First:    nil,
-		Second:   nil,
+		First:    day5PuzzleOne(copyStacks(stacks), moves),
+		Second:   day5PuzzleTwo(copyStacks(stacks), moves),
 		Duration: time.Now().Sub(begin),
 	}, nil
+}
+
+func day5PuzzleOne(stacks [][]string, moves []day5Move) string {
+	for _, move := range moves {
+		for i := 0; i < move.Count; i++ {
+			var (
+				src  = move.Source
+				dest = move.Destination
+			)
+			stacks[dest] = append(stacks[dest], stacks[src][len(stacks[src])-1])
+			stacks[src] = stacks[src][:len(stacks[src])-1]
+		}
+	}
+	var output string
+	for i := range stacks {
+		output += strings.NewReplacer("[", "", "]", "").Replace(stacks[i][len(stacks[i])-1])
+	}
+	return output
+}
+
+func day5PuzzleTwo(stacks [][]string, moves []day5Move) string {
+	for _, move := range moves {
+		var (
+			src  = move.Source
+			dest = move.Destination
+		)
+		stacks[dest] = append(stacks[dest], stacks[src][len(stacks[src])-move.Count:]...)
+		stacks[src] = stacks[src][:len(stacks[src])-move.Count]
+	}
+
+	var output string
+	for i := range stacks {
+		output += strings.NewReplacer("[", "", "]", "").Replace(stacks[i][len(stacks[i])-1])
+	}
+	return output
+}
+
+func copyStacks(stacks [][]string) [][]string {
+	var out = make([][]string, len(stacks))
+	for i := range stacks {
+		out[i] = []string{}
+		for e := range stacks[i] {
+			out[i] = append(out[i], stacks[i][e])
+		}
+	}
+	return out
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/bamsammich/advent-of-code-2022
 
 go 1.19
 
-require github.com/echojc/aocutil v0.0.0-20211125040159-e71bcb34f68f
+require (
+	github.com/echojc/aocutil v0.0.0-20211125040159-e71bcb34f68f
+	github.com/gosuri/uilive v0.0.4
+)
+
+require (
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+)
