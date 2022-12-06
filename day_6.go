41406e79ac0c8db4432ca74ed7228be5dd1f1e6f
Travis Huddleston <travis.huddleston@immuta.com>
2022-12-06 06:48:17 -0500

    day 6 solution

--- day_6.go
+++ day_6.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"time"
+
+	"github.com/echojc/aocutil"
 )
 
 func init() {
@@ -16,21 +19,49 @@ func (day *Day6Puzzle) Name() string {
 
 func (day *Day6Puzzle) Solution() (*Result, error) {
 	begin := time.Now()
-	// i, err := aocutil.NewInputFromFile("session_id")
-	// if err != nil {
-	// 	return nil, err
-	// }
+	i, err := aocutil.NewInputFromFile("session_id")
+	if err != nil {
+		return nil, err
+	}
+	data, err := i.Strings(2022, 6)
+	if err != nil {
+		return nil, err
+	}
+	stream := data[0]
 
-	// data, err := i.Strings(2022, 6)
-	// if err != nil {
-	// 	return nil, err
-	// }
+	var puzzleOne int
+	for i := 0; i+3 < len(stream); i++ {
+		packet := stream[i : i+4]
+		if unique([]rune(packet)) {
+			puzzleOne = i + 4
+			break
+		}
+	}
 
-	// fmt.Println(data)
+	var puzzleTwo int
+	for i := 0; i+13 < len(stream); i++ {
+		packet := stream[i : i+14]
+		if unique([]rune(packet)) {
+			puzzleTwo = i + 14
+			fmt.Println(packet)
+			break
+		}
+	}
 
 	return &Result{
-		First:    nil,
-		Second:   nil,
+		First:    puzzleOne,
+		Second:   puzzleTwo,
 		Duration: time.Now().Sub(begin),
 	}, nil
+}
+
+func unique[T comparable](sl []T) bool {
+	uniq := make(map[T]bool)
+	for _, e := range sl {
+		if ok := uniq[e]; ok {
+			return false
+		}
+		uniq[e] = true
+	}
+	return true
 }
