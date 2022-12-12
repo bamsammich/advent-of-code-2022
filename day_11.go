3469c7093ea4db7bb2463f953a917630e7a017dd
Travis Huddleston <travis.huddleston@immuta.com>
2022-12-12 08:08:44 -0500

    day 11 prep

---
+++ day_11.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/echojc/aocutil"
+)
+
+func init() {
+	puzzles = append(puzzles, &Day10Puzzle{})
+}
+
+type Day11Puzzle struct{}
+
+func (day *Day11Puzzle) Number() int {
+	return 11
+}
+
+func (day *Day11Puzzle) Name() string {
+	return fmt.Sprintf("day_%02d", day.Number())
+}
+
+func (day *Day11Puzzle) Solution() (*Result, error) {
+	begin := time.Now()
+	i, err := aocutil.NewInputFromFile("session_id")
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := i.Strings(2022, day.Number())
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(data)
+
+	return &Result{
+		First:    nil,
+		Second:   nil,
+		Duration: time.Now().Sub(begin),
+	}, nil
+}
