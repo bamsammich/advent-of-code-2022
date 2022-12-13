61c6da2ae856d0e189b058a952b2c120cdf70e30
Travis Huddleston <travis.huddleston@immuta.com>
2022-12-13 12:23:14 -0500

    day9 p2 working solution

--- day_9.go
+++ day_9.go
@@ -36,16 +36,37 @@ func (day *Day9Puzzle) Solution() (*Result, error) {
 	}
 
 	var (
-		rope = NewRope(9)
+		rope      = make([]Position, 10)
+		puzzleOne = make(map[Position]bool)
+		puzzleTwo = make(map[Position]bool)
 	)
 	for _, move := range data {
 		dir := strings.Split(move, " ")[0]
 		dist, _ := strconv.Atoi(strings.Split(move, " ")[1])
-		rope.Move(dir, dist)
+		for i := 0; i < dist; i++ {
+			switch dir {
+			case "U":
+				rope[0].Move(0, 1)
+			case "D":
+				rope[0].Move(0, -1)
+			case "R":
+				rope[0].Move(1, 0)
+			case "L":
+				rope[0].Move(-1, 0)
+			}
+			for i := range rope {
+				if i == 0 {
+					continue
+				}
+				rope[i].Toward(&rope[i-1])
+			}
+			puzzleOne[rope[1]] = true
+			puzzleTwo[rope[9]] = true
+		}
 	}
 	return &Result{
-		First:    len(rope.Tail.History),
-		Second:   len(rope.Tail.Last().History),
+		First:    len(puzzleOne),
+		Second:   len(puzzleTwo),
 		Duration: time.Now().Sub(begin),
 	}, nil
 }
@@ -59,8 +80,8 @@ func (pos *Position) Move(x, y int) {
 	pos.Y += y
 }
 
-func (pos *Position) Touches(other Position) bool {
-	xDiff, yDiff := pos.Diff(&other)
+func (pos *Position) Touches(other *Position) bool {
+	xDiff, yDiff := pos.Diff(other)
 	return xDiff >= -1 && xDiff <= 1 && yDiff >= -1 && yDiff <= 1
 }
 
@@ -87,27 +108,12 @@ func (pos *Position) String() string {
 	return fmt.Sprintf("{%d, %d}", pos.X, pos.Y)
 }
 
-type Rope struct {
-	Head *Position
-	Tail *Tail
-}
-
-func (r *Rope) String() string {
-	return fmt.Sprintf("Head: %s; Tail: %s", r.Head, r.Tail)
-}
-
-type Tail struct {
-	Position *Position
-	Next     *Tail
-	History  map[Position]bool
-}
-
-func (tail *Tail) Move(last *Position) {
-	if tail.Position.Touches(*last) {
+func (pos *Position) Toward(last *Position) {
+	if pos.Touches(last) {
 		return
 	}
 	x, y := 0, 0
-	dir := []rune(tail.Position.Direction(last))
+	dir := []rune(pos.Direction(last))
 	if contains(dir, 'N') {
 		y++
 	} else if contains(dir, 'S') {
@@ -118,66 +124,5 @@ func (tail *Tail) Move(last *Position) {
 	} else if contains(dir, 'W') {
 		x--
 	}
-	tail.Position.Move(x, y)
-	tail.History[*tail.Position] = true
-	if tail.Next != nil {
-		tail.Next.Move(tail.Position)
-	}
-}
-
-func (t *Tail) Last() *Tail {
-	if t.Next == nil {
-		return t
-	}
-	return t.Next
-}
-
-func (t *Tail) String() string {
-	if t.Next == nil {
-		return t.Position.String()
-	}
-	return fmt.Sprintf("%s, %s", t.Position, t.Next)
-}
-
-func (t *Tail) Index(i int) *Tail {
-	if i == 0 {
-		return t
-	}
-	i--
-	return t.Next.Index(i)
-}
-
-func (r *Rope) Move(direction string, distance int) {
-	for i := 0; i < distance; i++ {
-		switch direction {
-		case "U":
-			r.Head.Move(0, 1)
-		case "D":
-			r.Head.Move(0, -1)
-		case "R":
-			r.Head.Move(1, 0)
-		case "L":
-			r.Head.Move(-1, 0)
-		}
-		r.Tail.Move(r.Head)
-	}
-}
-
-func NewRope(tailLength int) *Rope {
-	return &Rope{
-		Head: &Position{0, 0},
-		Tail: NewTail(tailLength),
-	}
-}
-
-func NewTail(length int) *Tail {
-	startingPosition := Position{0, 0}
-	tail := Tail{
-		Position: &startingPosition,
-		History:  map[Position]bool{startingPosition: true},
-	}
-	if length > 1 {
-		tail.Next = NewTail(length - 1)
-	}
-	return &tail
+	pos.Move(x, y)
 }
