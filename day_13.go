d49f0d11bc3e120ffacb66bc68ed8cc5e67b668c
Travis Huddleston <travis.huddleston@immuta.com>
2022-12-18 10:57:18 -0500

    day 13 stup

--- day_13.go
+++ day_13.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
+
+	"github.com/echojc/aocutil"
 )
 
 func init() {
@@ -20,17 +23,27 @@ func (day *Day13Puzzle) Name() string {
 }
 
 func (day *Day13Puzzle) Solution() (*Result, error) {
-	// i, err := aocutil.NewInputFromFile("session_id")
-	// if err != nil {
-	// 	return nil, err
-	// }
+	i, err := aocutil.NewInputFromFile("session_id")
+	if err != nil {
+		return nil, err
+	}
 
-	// data, err := i.Strings(2022, day.Number())
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// fmt.Println(data)
+	data, err := i.Strings(2022, day.Number())
+	if err != nil {
+		return nil, err
+	}
 	begin := time.Now()
+
+	packets := make([][]any, 0)
+	for _, p := range data[0:2] {
+		var packet []any
+		if err := json.Unmarshal([]byte(p), &packet); err != nil {
+			return nil, err
+		}
+		packets = append(packets, packet)
+	}
+	fmt.Println(packets[0][0])
+	fmt.Println(packets[1][0])
 
 	return &Result{
 		First:    nil,
