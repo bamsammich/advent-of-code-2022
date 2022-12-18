53464a11c6f66b6ff73872a6b966ff5a63494293
Travis Huddleston <travis.huddleston@immuta.com>
2022-12-18 11:55:37 -0500

    day 13 solution

--- day_13.go
+++ day_13.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"sort"
+	"strings"
 	"time"
 
 	"github.com/echojc/aocutil"
@@ -28,26 +30,65 @@ func (day *Day13Puzzle) Solution() (*Result, error) {
 		return nil, err
 	}
 
-	data, err := i.Strings(2022, day.Number())
+	b, err := i.Bytes(2022, day.Number())
 	if err != nil {
 		return nil, err
 	}
 	begin := time.Now()
 
-	packets := make([][]any, 0)
-	for _, p := range data[0:2] {
-		var packet []any
-		if err := json.Unmarshal([]byte(p), &packet); err != nil {
-			return nil, err
+	var (
+		packets   = []any{}
+		puzzleOne = 0
+	)
+	for i, pair := range strings.Split(string(b), "\n\n") {
+		pairPackets := strings.Split(pair, "\n")
+		var left, right any
+		json.Unmarshal([]byte(pairPackets[0]), &left)
+		json.Unmarshal([]byte(pairPackets[1]), &right)
+		packets = append(packets, left, right)
+		if InOrder(left, right) <= 0 {
+			puzzleOne += i + 1
 		}
-		packets = append(packets, packet)
 	}
-	fmt.Println(packets[0][0])
-	fmt.Println(packets[1][0])
 
+	packets = append(packets, []any{[]any{2.}}, []any{[]any{6.}})
+	sort.Slice(packets, func(i, j int) bool { return InOrder(packets[i], packets[j]) < 0 })
+
+	puzzleTwo := 1
+	for i, p := range packets {
+		packetStr := fmt.Sprint(p)
+		if packetStr == "[[2]]" || packetStr == "[[6]]" {
+			puzzleTwo *= i + 1
+		}
+	}
 	return &Result{
-		First:    nil,
-		Second:   nil,
+		First:    puzzleOne,
+		Second:   puzzleTwo,
 		Duration: time.Now().Sub(begin),
 	}, nil
+}
+
+// Had trouble understanding how to normalize the lists. Got help from here:
+//
+//	https://github.com/mnml/aoc/blob/main/2022/13/1.go
+func InOrder(l, r any) int {
+	lSlice, lIsSlice := l.([]any)
+	rSlice, rIsSlice := r.([]any)
+
+	switch {
+	case !lIsSlice && !rIsSlice:
+		return int(l.(float64) - r.(float64))
+	case !lIsSlice:
+		lSlice = []any{l}
+	case !rIsSlice:
+		rSlice = []any{r}
+	}
+
+	for i := 0; i < len(lSlice) && i < len(rSlice); i++ {
+		if v := InOrder(lSlice[i], rSlice[i]); v != 0 {
+			return v
+		}
+	}
+
+	return len(lSlice) - len(rSlice)
 }
